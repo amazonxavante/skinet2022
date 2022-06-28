a458dadea21178777745c52bc6d42b85d930aac4
amazonxavante <williamvitorino@hotmail.com>
2022-06-28 12:19:13 +1000

    End of Section 11

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
+import { ErrorIntercetor } from './core/interceptors/error.interceptors';
+
+
 
 
 @NgModule({
@@ -21,8 +24,12 @@ import { HomeModule } from './home/home.module';
     HttpClientModule,
     CoreModule,
     HomeModule,
+    
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorIntercetor, multi: true}
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
